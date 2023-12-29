dda402bc85049c1349fb54a21ba26d3e2e0c5dbe
José Enrique <politeles@users.noreply.github.com>
2023-12-29 11:18:48 +0100

    updated outputs

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,17 @@ output "cluster_name" {
   description = "Kubernetes Cluster Name"
   value       = module.eks.cluster_name
 }
+
+output "cluster_id" {
+  description = "Kubernetes Cluster ID"
+  value       = module.eks.cluster_id
+}
+output "cluster_cert_data" {
+  description = "Kubernetes Cluster Certificate Data"
+  value       = module.eks.cluster_certificate_data
+}
+
+output "nodegroup_id" {
+  description = "Kubernetes Nodegroup ID"
+  value       = module.eks.cluster_nodegroup_id
+}
